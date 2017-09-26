2dd488bd43a98df5789e81e10845425e3cb9b251
Michael Læntver <brormand_96@hotmail.com>
2017-09-26 10:38:08 +0200

    hejsa

---
+++ SoapProviderCalculator/SoapProviderCalculator/IService1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapProviderCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        double Addition(double a, double b);
+        [OperationContract]
+        double Substract(double a, double b);
+        [OperationContract]
+        double Multiply(double a, double b);
+        [OperationContract]
+        double Division(double a, double b);
+        [OperationContract]
+        double Arithmetic(double a, double b, out double addition, out double substraction, out double multiply, out double division);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool _boolValue = true;
+        string _stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return _boolValue; }
+            set { _boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return _stringValue; }
+            set { _stringValue = value; }
+        }
+    }
+}
---
+++ SoapProviderCalculator/SoapProviderCalculator/Service1.svc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapProviderCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if(composite == null)
+            {
+                throw new ArgumentNullException(nameof(composite));
+            }
+            if(composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+        public double Arithmetic(double a, double b, out double addition, out double substraction, out double multiply, out double division)
+        {
+            addition = a + b;
+            substraction = a - b;
+            multiply = a * b;
+            division = a / b;
+            return addition + substraction + multiply + division;
+        }
+    }
+}
